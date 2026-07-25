9b786226e5c60bebaea4764b0257d7291d897098
harshdamani444-droid <harshdamani444@gmail.com>
2026-07-25 14:31:57 +0530

    Make getEnv helper completely crash-proof

--- backend/src/utils/get-env.ts
+++ backend/src/utils/get-env.ts
@@ -1,10 +1,7 @@
 export const getEnv = (key: string, defaultValue: string = ""): string => {
   const value = process.env[key];
-  if (value === undefined) {
-    if (defaultValue) {
-      return defaultValue;
-    }
-    throw new Error(`Enviroment variable ${key} is not set`);
+  if (!value || value.trim() === "") {
+    return defaultValue;
   }
   return value;
 };
